10d7342de40f96e1d65deaea3e8d9f84b0836326
Luis Diego Montero Vargas <115886062+DiegoMonteroCRC@users.noreply.github.com>
2025-03-28 23:20:39 -0600

    Propuestas

--- index.r
+++ index.r
@@ -1,1 +1,19 @@
-## main file
+##HIPOTESIS
+
+# Analizar concentraciones geofraficas por edad y actividad. (Mapa de calor, clustering)
+
+#Que tanta diferencia hay entre el GAM y fuera del GAM comparando las diferentes provincias
+#Es decir, ¿hay alguna diferencia entre las zonas urbanas o rurales o se mantienen de forma similar?
+
+
+##VIOLENCIA DOMESTICA
+
+####### HIPOTESIS
+#Exite alguna tendencia o mes donde los reportes de violencia se disparen
+
+#Si hay mas testimonios clave ayuda a cerrar mas casos?
+
+#Hay mas casos de violencia en zonas rurales que en zonas urbanas?
+
+##CORRELACION
+#Existe alguna CORRELACION ENTRE LOS TESTIMONIOS CLAVE contra los casos cerrados
