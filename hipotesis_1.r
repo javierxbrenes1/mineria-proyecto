09ad9d0ab37d519c52cfcf36aeb3a1dee7e60c67
= <=>
2025-04-15 00:32:46 -0600

    feat: more charts

--- hipotesis_1.r
+++ hipotesis_1.r
@@ -15,7 +15,7 @@ source("utilitarios.r")
 library(dplyr)
 library(stringr)
 library(stringi)
-
+library(ggplot2)
 options(width = 2000) 
 # Cargar datos de violencia doméstica
 datos_violencia <- cargar_datos_violencia()
@@ -30,6 +30,64 @@ datos_violencia <- datos_violencia %>% mutate(Provincia = clasificar_circuito(No
 
 
 # agregar flag si es area metropolitana (san jose, alajuela, heredia, cartago) o no (puntarenas, guanacaste, limon)
-datos_violencia <- datos_violencia %>% mutate(AreaMetropolitana = ifelse(Provincia %in% c("San Jose", "Alajuela", "Heredia", "Cartago"), 1, 0))
+datos_violencia <- datos_violencia %>% mutate(AreaMetropolitana = ifelse(Provincia %in% c("San Jose", "Alajuela", "Heredia", "Cartago"), 'SI', 'NO'))
+
+## Casos nuevos
+
+ggplot(datos_violencia, aes(x = AreaMetropolitana, fill = Entrados)) +
+  geom_bar() +
+    labs(title = "Distribución de casos de violencia doméstica por área metropolitana",
+       x = "Área Metropolitana",
+       y = "Número de casos Entrados", )
 
 
+ggplot(datos_violencia, aes(x = Provincia, fill = as.factor(AreaMetropolitana))) +
+  geom_bar() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  labs(title = "Distribución de casos de violencia doméstica por provincia y área metropolitana",
+       x = "Provincia",
+       y = "Número de casos")
+
+
+
+
+ggplot(datos_violencia, aes(x = Entrados, fill = AreaMetropolitana)) +
+  geom_histogram(position = "dodge", binwidth = 50) +
+ facet_wrap(~Anno) +
+  labs(
+    title = "Distribución de Casos Entrados por Área Metropolitana y Año",
+    x = "Número de Casos Entrados",
+    y = "Frecuencia",
+    fill = "Área Metropolitana"
+  ) +
+ theme_minimal() +
+  scale_fill_brewer(palette = "Set3") + 
+    scale_x_continuous(
+    breaks = seq(0, max(datos_violencia$Entrados), by = 100),  # Shows breaks every 100
+    labels = scales::comma  # Formats numbers with commas for readability
+  )
+
+
+## casos terminados
+
+ggplot(datos_violencia, aes(x = Terminasdos, fill = AreaMetropolitana)) +
+  geom_histogram(position = "dodge", binwidth = 50) +
+  facet_wrap(~Anno) +
+  labs(
+    title = "Distribución de Casos Terminados por Área Metropolitana y Año",
+    x = "Número de Casos Terminados",
+    y = "Frecuencia",
+    fill = "Área Metropolitana"
+  ) +
+  theme_minimal() +
+  scale_fill_brewer(palette = "Set3") + 
+    scale_x_continuous(
+    breaks = seq(0, max(datos_violencia$Terminasdos), by = 100),  # Shows breaks every 100
+    labels = scales::comma  # Formats numbers with commas for readability
+  )
+
+ggplot(datos_violencia, aes(x = AreaMetropolitana, fill = Terminasdos)) +
+  geom_bar() +
+    labs(title = "Distribución de casos terminados de violencia doméstica por área metropolitana",
+       x = "Área Metropolitana",
+       y = "Número de casos Terminados", )
