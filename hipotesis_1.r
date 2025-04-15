b317c13c792aed3db42b27c24960169f41243eb8
= <=>
2025-04-14 23:45:39 -0600

    feat: start working on hyp 1

--- hipotesis_1.r
+++ hipotesis_1.r
@@ -1,24 +1,35 @@
 # 1. Distribución Geográfica de la Violencia Doméstica:
-
-# Pregunta de Investigación:
-# ¿Existe una variación significativa en la incidencia de casos de violencia doméstica entre áreas urbanas y rurales?
+# Pregunta de Investigación: 
+# ¿Existe una variación significativa en la incidencia de casos de violencia doméstica entre áreas del gran área metropolitana y áreas fuera del área metropolitana?
 
 # Objetivos:
 # Analizar la distribución geográfica de los casos nuevos y terminados de violencia doméstica por circuito judicial.
-# Realizar una comparación de la incidencia de casos entre zonas urbanas y rurales.
+# Realizar una comparación de la incidencia de casos entre áreas del gran área metropolitana y áreas fuera del área metropolitana
 
 # Requerimientos de Datos:
 # Depuración y homogeneización del conjunto de datos existente.
-# Enriquecimiento del conjunto de datos para permitir la clasificación de los circuitos judiciales en zonas urbanas y rurales.
+# Enriquecimiento del conjunto de datos para permitir la clasificación de los circuitos judiciales en áreas del gran área metropolitana y áreas fuera del área metropolitana.
 
 # Importar funciones utilitarias
 source("utilitarios.r")
+library(dplyr)
+library(stringr)
+library(stringi)
+
 options(width = 2000) 
 # Cargar datos de violencia doméstica
 datos_violencia <- cargar_datos_violencia()
 
 # Verificar la estructura de los datos
 str(datos_violencia)
-head(datos_violencia)
+head(datos_violencia, 1)
+datos_violencia$NombreCircuito
+
+# agregar la provincia como parte del dataSet
+datos_violencia <- datos_violencia %>% mutate(Provincia = clasificar_circuito(NombreCircuito))
+
+
+# agregar flag si es area metropolitana (san jose, alajuela, heredia, cartago) o no (puntarenas, guanacaste, limon)
+datos_violencia <- datos_violencia %>% mutate(AreaMetropolitana = ifelse(Provincia %in% c("San Jose", "Alajuela", "Heredia", "Cartago"), 1, 0))
 
 
--- utilitarios.r
+++ utilitarios.r
@@ -1,3 +1,73 @@
+# circuitos_provincias <- data.frame(
+#   circuito = c(
+#     "I Circuito Judicial de San José",
+#     "II Circuito Judicial de San José",
+#     "III Circuito Judicial de San José",
+#     "I Circuito Judicial de la Zona Sur",
+#     "II Circuito Judicial de la Zona Sur",
+#     "I Circuito Judicial de Alajuela",
+#     "II Circuito Judicial de Alajuela",
+#     "III Circuito Judicial de Alajuela",
+#     "Circuito Judicial de Cartago",
+#     "Circuito Judicial de Heredia",
+#     "I Circuito Judicial de Guanacaste",
+#     "II Circuito Judicial de Guanacaste",
+#     "Circuito Judicial de Puntarenas",
+#     "I Circuito Judicial de la Zona Atlántica",
+#     "II Circuito Judicial de la Zona Atlántica"
+#   ),
+#   provincia = c(
+#     "San Jose",
+#     "San Jose",
+#     "San Jose",
+#     "Puntarenas",
+#     "Puntarenas",
+#     "Alajuela",
+#     "Alajuela",
+#     "Alajuela",
+#     "Cartago",
+#     "Heredia",
+#     "Guanacaste",
+#     "Guanacaste",
+#     "Puntarenas",
+#     "Limon",       # Zona Atlántica is Limón
+#     "Limon"        # Zona Atlántica is Limón
+#   )
+# )
+
+circuitos_provincias <- data.frame(
+    circuito = limpiar_texto(c(
+        'San Jose',
+        'Heredia',
+        'Alajuela',
+        'Cartago',
+        'Guanacaste',
+        'Puntarenas',
+        'Limon',
+        'ATLÁNTICA',
+        'Sur',
+        'ATENAS',
+        'Turrialba',
+        'Golfito'
+        
+    )),
+    provincia = c(
+        'San Jose',
+        'Heredia',
+        'Alajuela',
+        'Cartago',
+        'Guanacaste',
+        'Puntarenas',
+        'Limon',
+        'Limon',
+        'Puntarenas',
+        'Alajuela',
+        'Cartago',
+        'Puntarenas'
+    )
+)
+
+circuitos_provincias
 
 # Function to load domestic violence data
 cargar_datos_violencia <- function(ruta = "data_sources/ARCHIVO_VIOLENCIA_DOMESTICA.tsv") {
@@ -15,4 +85,21 @@ cargar_datos_violencia <- function(ruta = "data_sources/ARCHIVO_VIOLENCIA_DOMEST
                      sep = '\t')
 
     return(datos)
+}
+
+limpiar_texto <- function(texto) {
+  texto <- tolower(texto)
+  texto <- stri_trans_general(texto, "Latin-ASCII")
+  texto <- str_trim(texto)
+  return(texto)
+}
+
+clasificar_circuito <- function(circuitos) {
+    resultados <- rep(NA_character_, length(circuitos))
+    
+    for(i in seq_len(nrow(circuitos_provincias))) {
+        matches <- str_detect(limpiar_texto(circuitos), limpiar_texto(circuitos_provincias$circuito[i]))
+        resultados[matches] <- circuitos_provincias$provincia[i]
+    }
+    return(resultados)
 }
